760f2302072861ab458052ea4c6e104299e34d15
Katrina Rosales <katrinarosales@icloud.com>
2025-02-03 14:26:42 +0100

    Altered the path for main.ts file and fixed the bug for favicons

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/kcs/' : '/'
-  }
+    publicPath: process.env.NODE_ENV === 'production' ? '/kcs/' : '/',
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'Katrinas Coding Studio',
+        },
+    },
+}
